25f52c9b373b51892bba4150287b263fb12a2a90
Oleksandra75 <j7neo2312@gmail.com>
2023-08-31 12:21:52 +0300

    add alt

--- js/js.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener('scroll', function(){
-   scrollY > 0 ? document.querySelector('.header').classList.add('scroll'):this.document.querySelector('header').classList.remove('scroll');
-});
